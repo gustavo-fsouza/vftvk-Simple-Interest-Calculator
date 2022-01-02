904660da08b169e902f0d00dd0fbd0c7e4e8b7f0
gustavo-fsouza <gustavo.fsouza@hotmail.com>
2022-01-02 18:28:30 -0300

    novas alteracoes

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
 function compute() {
+    
     capital = document.getElementById("capital").value;
     rate = document.getElementById("rate").value;
     years = document.getElementById("years").value;
-    console.log(years);
 
-    currentYear = parseInt(new Date().getFullYear());
-    // converter string para int
-    years = parseInt(years);
-    rate = parseFloat(rate);
-    capital = parseInt(capital);
+    if (capital == "") {
+        alert("Please fill the amount field with a number")
+        capital.focus();
+        return false;
+    }
+    else if (years == "") {
+        alert("Please fill the years field with a number")
+        years.focus();
+        return false;
+    }
+    else {
+        currentYear = parseInt(new Date().getFullYear());
 
-    amount = years * (rate / 100) * capital;
-    years = currentYear + years;
+        // converter string para int
+        years = parseInt(years);
+        rate = parseFloat(rate);
+        capital = parseFloat(capital);
+
+        amount = years * (rate / 100) * capital;
+
+        // float ficara sempre com duas casas decimais
+        amount = amount.toFixed(2);
+
+
+        years = currentYear + years;
+
+
+        //criando uma nova div a partir da funcao
+        var NewDiv = document.createElement("div");
+        // inserindo código html na nova div
+        NewDiv.innerHTML = "<p>If you deposit: " + capital + "</br>"
+            + "at an Interest rate of " + rate + "%</br>"
+            + "You will receive an amount of " + amount + "</br>"
+            + "in the year " + years + "</p>";
+        // criando vasriavel container para dizer onde quero que as divs sejam incluidas no html
+        const DivContainer = document.getElementById("DivContainer")
+        // adicionando essa div no body do documento
+        // document.body.appendChild(NewDiv);
+        DivContainer.appendChild(NewDiv)
+    }
     
-
-    //criando uma nova div a partir da funcao
-    var NewDiv = document.createElement("div");
-    // inserindo código html na nova div
-    NewDiv.innerHTML = "<p>If you deposit: "+ capital +"</br>"
-        + "at an Interest rate of " + rate + "%</br>"
-        + "You will receive an amount of " + amount +"</br>"
-        + "in the year " + years +"</p>";
-    // criando vasriavel container para dizer onde quero que as divs sejam incluidas no html
-    const DivContainer = document.getElementById("DivContainer")
-    // adicionando essa div no body do documento
-    // document.body.appendChild(NewDiv);
-    DivContainer.appendChild(NewDiv)
+    
 }
 
-function updateTextInput(val) {
-    document.getElementById('rate').value = val;
+function RateShow() {
+    rate_p.value = document.getElementById("rate").value;
 }
